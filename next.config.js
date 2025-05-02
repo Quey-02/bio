379d74a5a97cfb7b66ece17c42348dbf95400ca8
kyui <ibuki021210@outlook.jp>
2025-05-03 01:47:18 +0900

    Revert "check prefix"
    This reverts commit f353f46e4faeb3e80c9d02ced184815bcbebbdac.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+}
 
-// module.exports = nextConfig
-
-
-
-
-const isGithubPages = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  assetPrefix: isGithubPages ? '/bio/' : '',
-  basePath: isGithubPages ? '/bio' : '',
-  trailingSlash: true,
-  output: 'export',
-};
+module.exports = nextConfig
