f353f46e4faeb3e80c9d02ced184815bcbebbdac
kyui <ibuki021210@outlook.jp>
2025-05-03 01:40:00 +0900

    check prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+
+
+const isGithubPages = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isGithubPages ? '/bio/' : '',
+  basePath: isGithubPages ? '/bio' : '',
+  trailingSlash: true,
+  output: 'export',
+};
