93a6d693570b4ac9dbd5039e52c3e2743e3eea08
Kyui <g.ibu.ki.kara.165.namiki@gmail.com>
2025-05-20 15:39:40 +0900

    refactor: resolve redundant nest of elements

--- src/components/TechStackAccordion.tsx
+++ src/components/TechStackAccordion.tsx
@@ -11,44 +11,33 @@ export function TechStackAccordion({
   field: ProgrammingLanguageProficiency;
 }) {
   return (
-    <div key={field.name} className={clsx(styles["tech-item"])}>
-      <Accordion.Root
-        type="single"
-        collapsible
-        className={clsx(styles["accordion"])}
-        defaultValue="item-1"
+    <Accordion.Item value={field.name} className={clsx(styles["tech-item"])}>
+      <Accordion.Header className={clsx(styles["accordion-header"])}>
+        <Accordion.Trigger
+          className={clsx(styles["accordion-trigger"], styles["caption"])}
+        >
+          <div className={clsx(styles["field-name"])}>{field.name}</div>
+          <ChevronDownIcon
+            className={clsx(styles["accordion-chervon"])}
+            aria-hidden
+          />
+        </Accordion.Trigger>
+      </Accordion.Header>
+      <Accordion.Content
+        className={clsx(styles["content"], styles["module-list"])}
       >
-        <Accordion.Item value="item-1">
-          <Accordion.Header className={clsx(styles["header"])}>
-            <Accordion.Trigger className={clsx(styles["accordion-trigger"])}>
-              <div className={clsx(styles["caption"])}>
-                <div className={clsx(styles["field-name"])}>{field.name}</div>
-                <ChevronDownIcon
-                  className={clsx(styles["accordion-chervon"])}
-                  aria-hidden
-                />
-              </div>
-            </Accordion.Trigger>
-            <Accordion.Content
-              className={clsx(styles["content"], styles["module-list"])}
-            >
-              {field.modules.map((module) => (
-                <div key={module.name} className={clsx(styles["module-item"])}>
-                  <img
-                    src={module.icon_uri}
-                    alt={module.name}
-                    className={clsx(styles["module-icon"])}
-                  />
-                  <div className={clsx(styles["module-name"])}>
-                    {module.name}
-                  </div>
-                </div>
-              ))}
-            </Accordion.Content>
-          </Accordion.Header>
-        </Accordion.Item>
-      </Accordion.Root>
-    </div>
+        {field.modules.map((module) => (
+          <div key={module.name} className={clsx(styles["module-item"])}>
+            <img
+              src={module.icon_uri}
+              alt={module.name}
+              className={clsx(styles["module-icon"])}
+            />
+            <div className={clsx(styles["module-name"])}>{module.name}</div>
+          </div>
+        ))}
+      </Accordion.Content>
+    </Accordion.Item>
   );
 }
 
--- src/components/TechStackList.tsx
+++ src/components/TechStackList.tsx
@@ -5,15 +5,16 @@ import styles from "./TechStackList.module.scss";
 import techStack from "@/data/tech-stack.json";
 import { TechStack, ProgrammingLanguageProficiency } from "@/types/main";
 import TechStackAccordion from "./TechStackAccordion";
+import { Accordion } from "radix-ui";
 
 export default function TechStackList() {
   return (
-    <div className={clsx(styles["tech-stack-list"])}>
+    <Accordion.Root type="multiple" className={clsx(styles["tech-stack-list"])}>
       {(techStack as TechStack).fields.map(
         (field: ProgrammingLanguageProficiency, fieldIdx) => (
           <TechStackAccordion key={fieldIdx} field={field} />
         )
       )}
-    </div>
+    </Accordion.Root>
   );
 }
