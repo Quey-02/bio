3705f746de21e01a4f2474f113b50a98c4dbc0fe
Kyui <g.ibu.ki.kara.165.namiki@gmail.com>
2025-08-20 13:54:26 +0900

    feat: add github link

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -1,8 +1,31 @@
 "use client";
 
+import clsx from "clsx";
 
-export default function SocialPage() {
+import styles from "./page.module.scss";
+
+export default function Page() {
+  // const router = useRouter();
+
   return (
-    <div>工事中</div>
+    <>
+      <div>工事中</div>
+
+      <button
+        className={clsx(styles["github-link"])}
+        onClick={() => {
+          window.open("https://github.com/Quey-02", "_blank");
+        }}
+      >
+        <div className={clsx(styles["github-link-item"])}>
+          <img
+            src="github.svg"
+            alt="github"
+            className={clsx(styles["github-icon"])}
+          />
+          <div className={clsx(styles["github-link-text"])}>@Quey-02</div>
+        </div>
+      </button>
+    </>
   );
 }
